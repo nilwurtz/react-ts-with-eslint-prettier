7334e9a7bc1339af2f68b1c50410a399717ae6a1
ragnar1904 <apollo.oth@gmail.com>
2020-11-18 18:27:56 +0900

    add eslint + prettier from template

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Index Page</h1>
-            <p>This is React + Typescript minimum template.</p>
-            <a href="https://github.com/ragnar1904/react-ts-min">Github</a>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Index Page</h1>
+      <p>This is React + Typescript + eslint + prettier template.</p>
+      <a href="https://github.com/ragnar1904/react-ts-with-eslint-prettier">
+        Github
+      </a>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
